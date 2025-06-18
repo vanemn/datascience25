0b901829bd29211b73d17588b39987dde73aa79c
Vane <vane.moralesn@gmail.com>
2025-06-17 20:49:50 -0400

    actividad modulo 3 importar csv y excel

---
+++ mod3/s_practica1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# # Cargar el archivo CSV
+# df = pd.read_csv('entradas/estudiantes.csv',sep=";")
+# print(df)
+
+
+# #eliminar filas con valores nulos
+# df = pd.read_csv('entradas/ventas.csv', na_values=["N/A", "n/a", "NA", "na"])
+# df = df.dropna()
+# print(df)
+
+# # Guardar el DataFrame en un archivo CSV
+# # data = {"producto":["manzana", "pera", "naranja", "plátano"],"Precio":[1.2, 0.8, 1.5, 1.0],"Cantidad":[10, 20, 15, 12]}
+# df = pd.DataFrame(data)
+# df.to_csv('entradas/productos.csv', index=False, sep=";")
+
+
