e9559c12c5d329b67b8c4a2c5b28266d1cc5e3c1
Vane <vane.moralesn@gmail.com>
2025-06-24 20:04:41 -0400

    ejercicio de multi_index para ajustar la vista del df

--- mod3/practica_8/practica8.py
+++ mod3/practica_8/practica8.py
@@ -8,11 +8,11 @@ df_productos = pd.read_csv('productos_categoria.csv')
 print("\n====== Vista original de  DataFrames ======\n")
 print(df_ventas.head(9))   #puedo indicar el número de filas que quiero ver o dejar vacío 5
 df_multi_index = df_ventas.set_index(['Sucursal', 'Fecha', 'Categoria'])
-print("\n====== Vista de df set_index agrupado  ======\n")
+print("\n====== Vista de df set_index   ======\n")
 print(df_multi_index.head(9))
-# Vista de df completo ventas
+
 print("\n====== Vista completa de df ventas ======\n")
 print(df_ventas)
-# Vista completa de df_multi_index
+
 print("\n====== Vista completa de df_multi_index ======\n")
 print(df_multi_index)
